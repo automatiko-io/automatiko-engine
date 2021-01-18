1c52d3240718eab72ad185eeef4da1f31ff0494a
Maciej Swiderski <swiderski.maciej@gmail.com>
2021-01-18 11:15:05 +0100

    fixed javadocs

--- api/automatiko-engine-api/src/main/java/io/automatiko/engine/api/io/ResourceConfiguration.java
+++ api/automatiko-engine-api/src/main/java/io/automatiko/engine/api/io/ResourceConfiguration.java
@@ -3,16 +3,10 @@ package io.automatiko.engine.api.io;
 
 import java.util.Properties;
 
-/**
- * This interface is a marker interface and should be implemented by any class
- * that will provide configurations to the
- * {@link org.kie.api.builder.KieBuilder} - currently this is only used by
- * decision tables.
- */
 public interface ResourceConfiguration {
 
-	public Properties toProperties();
+    public Properties toProperties();
 
-	public ResourceConfiguration fromProperties(Properties prop);
+    public ResourceConfiguration fromProperties(Properties prop);
 
 }
