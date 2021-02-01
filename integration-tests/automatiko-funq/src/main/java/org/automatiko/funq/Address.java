b1803f3b6e56b16b9aae28d64d9de0550b10d113
Maciej Swiderski <swiderski.maciej@gmail.com>
2021-02-01 14:05:11 +0100

    provide instructions for Workflow as a Function Flow, provide instructions for Workflow as a Function

---
+++ integration-tests/automatiko-funq/src/main/java/org/automatiko/funq/Address.java
@@ -0,0 +1,33 @@
+package org.automatiko.funq;
+
+public class Address {
+
+    private String street;
+    private String city;
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
