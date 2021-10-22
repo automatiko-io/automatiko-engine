3bd1c779e0a8ccbc3ff5ad7efeecd75a5b1cd40a
Maciej Swiderski <swiderski.maciej@gmail.com>
2021-10-22 14:11:32 +0200

    refactored archive service on top of files addon to properly support it, ignore null content files by files addons

--- api/automatiko-engine-api/src/main/java/io/automatiko/engine/api/workflow/files/File.java
+++ api/automatiko-engine-api/src/main/java/io/automatiko/engine/api/workflow/files/File.java
@@ -3,11 +3,16 @@ package io.automatiko.engine.api.workflow.files;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public interface File<T> {
 
     public static final String DEFAULT_CONTENT_TYPE = "application/octet-stream";
+
+    public static final String ENTRIES_ATTR = "entries";
 
     /**
      * Returns name of the file including extension if defined
@@ -47,6 +52,23 @@ public interface File<T> {
      * @return url of the file
      */
     String url();
+
+    /**
+     * Return entries of the file - depending on the type of file it can be populated or empty list
+     * 
+     * @return found entries in the file
+     */
+    default List<String> entries() {
+        if (attributes() != null) {
+            String entries = attributes().get(ENTRIES_ATTR);
+
+            if (entries != null) {
+
+                return Arrays.asList(entries.split(","));
+            }
+        }
+        return Collections.emptyList();
+    }
 
     public static String discoverType(String name) {
         try {
