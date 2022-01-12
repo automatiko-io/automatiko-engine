838e62dc2754a0bed7bf480c2f0d6d825a1707fa
Maciej Swiderski <swiderski.maciej@gmail.com>
2022-01-12 14:53:15 +0100

    upgrade to quarkus 2.6.2 and its dependencies, upgrade of camal quarkus to 2.6.0

--- integration-tests/automatiko-kafka/src/test/java/org/acme/home/VerificationTest.java
+++ integration-tests/automatiko-kafka/src/test/java/org/acme/home/VerificationTest.java
@@ -21,10 +21,10 @@ import io.automatiko.engine.api.event.EventPublisher;
 import io.automatiko.engine.services.event.impl.CountDownProcessInstanceEventPublisher;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
-import io.smallrye.reactive.messaging.connectors.InMemorySink;
-import io.smallrye.reactive.messaging.connectors.InMemorySource;
 import io.smallrye.reactive.messaging.kafka.KafkaRecord;
+import io.smallrye.reactive.messaging.providers.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.providers.connectors.InMemorySink;
+import io.smallrye.reactive.messaging.providers.connectors.InMemorySource;
 
 @QuarkusTest
 public class VerificationTest {
