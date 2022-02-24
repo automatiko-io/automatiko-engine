54675a090cece9c2f0e0fcd3b9397cb585f6b5cb
Maciej Swiderski <swiderski.maciej@gmail.com>
2022-02-24 17:54:51 +0100

    improved operator archetype to create initial workflow and classes out of the box

---
+++ archetypes/automatiko-operator-archetype/src/main/resources/archetype-resources/src/main/java/__classNameOfResource__.java
@@ -0,0 +1,22 @@
+package ${package};
+
+import io.fabric8.kubernetes.api.model.Namespaced;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Group("\${groupId}")
+@Version("v1alpha1")
+public class ${classNameOfResource} extends CustomResource<${classNameOfSpec}, ${classNameOfStatus}> implements Namespaced {
+
+    @Override
+    protected ${classNameOfSpec} initSpec() {
+        return new ${classNameOfSpec}();
+    }
+
+    @Override
+    protected ${classNameOfStatus} initStatus() {
+        return new ${classNameOfStatus}();
+    }
+
+}
