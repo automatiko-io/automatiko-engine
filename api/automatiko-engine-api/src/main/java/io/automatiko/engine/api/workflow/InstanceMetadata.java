d1136b12b4987589ca79a96542895ba5d8ffae3e
Maciej Swiderski <swiderski.maciej@gmail.com>
2022-03-25 08:19:17 +0100

    added links to task that are available for user performing request to metadata to cover 'next step' use case

--- api/automatiko-engine-api/src/main/java/io/automatiko/engine/api/workflow/InstanceMetadata.java
+++ api/automatiko-engine-api/src/main/java/io/automatiko/engine/api/workflow/InstanceMetadata.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import io.automatiko.engine.api.auth.IdentityProvider;
+import io.automatiko.engine.api.auth.SecurityPolicy;
+
 public class InstanceMetadata {
 
     private String id;
@@ -15,15 +18,19 @@ public class InstanceMetadata {
 
     private Set<String> tags;
 
+    private Set<Link> links;
+
     public InstanceMetadata() {
     }
 
-    private InstanceMetadata(String id, String businessKey, String description, int state, Collection<String> tags) {
+    private InstanceMetadata(String id, String businessKey, String description, int state, Collection<String> tags,
+            Set<Link> links) {
         this.id = id;
         this.businessKey = businessKey;
         this.description = description;
         this.state = state;
         this.tags = tags == null ? Collections.emptySet() : new HashSet<>(tags);
+        this.links = links == null ? Collections.emptySet() : links;
     }
 
     public String getId() {
@@ -66,8 +73,81 @@ public class InstanceMetadata {
         this.tags = tags;
     }
 
+    public Set<Link> getLinks() {
+        return links;
+    }
+
+    public void setLinks(Set<Link> links) {
+        this.links = links;
+    }
+
     public static InstanceMetadata of(ProcessInstance<?> instance) {
+        Set<Link> links = new HashSet<>();
+        for (WorkItem task : instance.workItems(SecurityPolicy.of(IdentityProvider.get()))) {
+            Link link = new Link("task", task.getName(), task.getReferenceId(), task.getFormLink());
+            links.add(link);
+        }
+
         return new InstanceMetadata(instance.id(), instance.businessKey(), instance.description(), instance.status(),
-                instance.tags().values());
+                instance.tags().values(), links);
+    }
+
+    public static class Link {
+        private String type;
+
+        private String name;
+
+        private String url;
+
+        private String form;
+
+        public Link() {
+
+        }
+
+        public Link(String type, String name, String url, String formLink) {
+            this.type = type;
+            this.name = name;
+            this.url = url;
+            this.form = formLink;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getForm() {
+            return form;
+        }
+
+        public void setForm(String form) {
+            this.form = form;
+        }
+
+        @Override
+        public String toString() {
+            return "Link [type=" + type + ", name=" + name + ", url=" + url + "]";
+        }
+
     }
 }
