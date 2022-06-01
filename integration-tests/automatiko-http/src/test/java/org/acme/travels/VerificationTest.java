d1586a8ded8d33e0362bc7466c8712367ab96d80
Maciej Swiderski <swiderski.maciej@gmail.com>
2022-06-01 09:37:57 +0200

    test coverage for support of expression for message event data mapping, persistence addon discovery and fixed child process in different package

--- integration-tests/automatiko-http/src/test/java/org/acme/travels/VerificationTest.java
+++ integration-tests/automatiko-http/src/test/java/org/acme/travels/VerificationTest.java
@@ -88,5 +88,79 @@ public class VerificationTest {
             .body("$.size()", is(0));
     }
     
+    @Test
+    public void testProcessSendsWithExpressionAndReceives() throws InterruptedException {
+        String id = "bbb";
+        String addPayload = "{\n"
+                + "  \"person\": {\n"
+                + "    \"name\": \"john\",\n"
+                + "    \"age\": 41\n"
+                + "  }\n"
+                + "}";
+        given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(addPayload)
+            .when()
+                .post("/sendsexp?businessKey=" + id)
+            .then()
+                //.log().body(true)
+                .statusCode(200)
+                .body("id", notNullValue(), "person.name", equalTo("john"), "person.age", equalTo(41));
+        
+        given()
+            .accept(ContentType.JSON)
+        .when()
+            .get("/sendsexp")
+        .then().statusCode(200)
+            .body("$.size()", is(0));
+        
+        long elapsed = 3000;
+        
+        while(elapsed >= 0) {
+            elapsed -= 1000;
+            Thread.sleep(1000);
+            int size = given()
+                .accept(ContentType.JSON)
+            .when()
+                .get("/recivers")
+            .then().statusCode(200)
+                .extract().path("$.size()");
+            
+            if (size == 1) {
+                break;
+            }
+        }
+        
+        String receiversId = given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body(addPayload)
+            .when()
+                .get("/recivers")
+            .then()
+                //.log().body(true)
+                .statusCode(200)
+                .body("[0].id", notNullValue(), "[0].person.name", equalTo("JOHN"), "[0].person.age", equalTo(51))
+                .extract().path("[0].id");
+        
+        given()
+        .contentType(ContentType.JSON)
+        .accept(ContentType.JSON)
+        .body(addPayload)
+        .when()
+            .delete("/recivers/"+receiversId)
+        .then()
+            //.log().body(true)
+            .statusCode(200);
+        
+        given()
+            .accept(ContentType.JSON)
+        .when()
+            .get("/recivers")
+        .then().statusCode(200)
+            .body("$.size()", is(0));
+    }
+    
  // @formatter:on
 }
